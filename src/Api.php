1e5fdddac95d2437665f30726896a83a3aca91c0
Ananta Ram das <anantaramdas@outlook.com>
2018-02-28 14:35:21 +0200

    Added Api class with constants

---
+++ src/Api.php
@@ -0,0 +1,10 @@
+<?php
+
+class Api
+{
+    const BILLINGPERIOD_DAY         = 'Day';
+    const BILLINGPERIOD_WEEK        = 'Week';
+    const BILLINGPERIOD_SEMIMONTH   = 'SemiMonth';    // For SemiMonth, billing is done on the 1st and 15th of each month.
+    const BILLINGPERIOD_MONTH       = 'Month';
+    const BILLINGPERIOD_YEAR        = 'Year';
+}
